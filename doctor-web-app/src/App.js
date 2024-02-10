4b68c4b47c88b12f819bc47f37ef5847fe5ce0da
Abhishek Gupta <abhig0209@gmail.com>
2024-02-10 12:27:53 +0530

    navbar done

--- doctor-web-app/src/App.js
+++ doctor-web-app/src/App.js
@@ -1,27 +1,30 @@
-import './App.css';
+import "./App.css";
 
-import DoctorChat from './pages/DoctorChat';
-import SearchPatient from './pages/SearchPatient';
-import Profile from './pages/Profile';
-import DoctorDashboard from './pages/DoctorDashboard';
-import PageNotFound from './pages/PageNotFound';
+import DoctorChat from "./pages/DoctorChat";
+import SearchPatient from "./pages/SearchPatient";
+import Profile from "./pages/Profile";
+import DoctorDashboard from "./pages/DoctorDashboard";
+import PageNotFound from "./pages/PageNotFound";
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <Router>
-        <Routes>
-          <Route path="/" element={<DoctorDashboard />} />
-          <Route path="search-patient" element={<SearchPatient />} />
-          <Route path="doctor-chat" element={<DoctorChat />} />
-          <Route path="profile" element={<Profile />} />
-          <Route path="*" element={<PageNotFound />} />
-      </Routes>
-    </Router>
-    </div>
-  );
+	return (
+		<div className="App">
+			<Router>
+				<Routes>
+					<Route path="/" element={<DoctorDashboard />} />
+					<Route
+						path="search-patient"
+						element={<SearchPatient />}
+					/>
+					<Route path="doctor-chat" element={<DoctorChat />} />
+					<Route path="profile" element={<Profile />} />
+					<Route path="*" element={<PageNotFound />} />
+				</Routes>
+			</Router>
+		</div>
+	);
 }
 
 export default App;
--- doctor-web-app/src/components/misc/Navbar.js
+++ doctor-web-app/src/components/misc/Navbar.js
@@ -1,7 +1,138 @@
+// import "../css/navbar.css";
+// import { useEffect } from "react";
+
+// function Navbar() {
+
+// 	return (
+// 		<nav id="nav">
+// 			<div className="nav-logo">
+// 				<div className="nav-heading">
+// 					<span href="#">
+// 						<img
+// 							id="logo"
+// 							src={require(`../../static/logo.png`)}
+// 							alt=""
+// 						/>
+// 						Swasth Sahayak
+// 					</span>
+// 				</div>
+
+// 				<div className="hamburger">
+// 					<a href="#">
+// 						<i className="fas fa-bars "></i>
+// 					</a>
+// 				</div>
+// 			</div>
+
+// 			<ul className="nav-links">
+// 				<li>
+// 					<a className="nav-item" href="/">
+// 						Home
+// 					</a>
+// 				</li>
+// 				<li>
+// 					<a className="nav-item" href="/">
+// 						Search Patient
+// 					</a>
+// 				</li>
+// 				<li>
+// 					<a className="nav-item" href="/">
+// 						Chat with Doctor
+// 					</a>
+// 				</li>
+// 				<li>
+// 					<a className="nav-item" href="/">
+// 						Profile
+// 					</a>
+// 				</li>
+// 			</ul>
+// 		</nav>
+// 	);
+// }
+
+// export default Navbar;
+
+import React, { useState, useEffect } from "react";
+import "../css/navbar.css";
+
 function Navbar() {
-    return (
-        <div></div>
-    )
+	const [showNavLinks, setShowNavLinks] = useState(false);
+	const [newNav, setNewNav] = useState(false);
+
+	const handleScroll = () => {
+		const scroll = window.scrollY;
+		const width = window.innerWidth;
+
+		if (scroll > 10 && width > 995) {
+			setNewNav(true);
+		} else if (scroll === 0 && width > 995) {
+			setNewNav(false);
+		}
+
+		if (scroll >= 70 && width < 995) {
+			setNewNav(true);
+		} else if (scroll === 0 && width < 995) {
+			setNewNav(false);
+		}
+	};
+
+	const handleToggle = () => {
+		setShowNavLinks(!showNavLinks);
+	};
+
+	useEffect(() => {
+		window.addEventListener("scroll", handleScroll);
+		return () => {
+			window.removeEventListener("scroll", handleScroll);
+		};
+	}, []);
+
+	return (
+		<nav className={`nav ${newNav ? "new-nav" : ""}`}>
+			<div className="nav-logo">
+				<div className="nav-heading">
+					<span href="#">
+						<img
+							id="logo"
+							src={require(`../../static/logo.png`)}
+							alt=""
+						/>
+						Swasth Sahayak
+					</span>
+				</div>
+
+				<div className="hamburger" onClick={handleToggle}>
+					<i className="fas fa-bars"></i>
+				</div>
+			</div>
+
+			<ul
+				className="nav-links"
+				style={{ display: showNavLinks ? "block" : "none" }}
+			>
+				<li>
+					<a className="nav-item" href="/">
+						Home
+					</a>
+				</li>
+				<li>
+					<a className="nav-item" href="/">
+						Search Patient
+					</a>
+				</li>
+				<li>
+					<a className="nav-item" href="/">
+						Chat with Doctor
+					</a>
+				</li>
+				<li>
+					<a className="nav-item" href="/">
+						Profile
+					</a>
+				</li>
+			</ul>
+		</nav>
+	);
 }
 
 export default Navbar;
