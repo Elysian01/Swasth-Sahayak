6baaf0bc44356b7c62d679d65d1f824e53d4c258
Farman Ahmed <farmanahmed888@gmail.com>
2024-02-12 23:27:54 +0530

    added calendar picker component

---
+++ doctor-web-app/src/components/misc/Calendar.js
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+
+function Calendar() {
+  const [selectedDate, setSelectedDate] = useState(null);
+
+  const handleDateChange = (date) => {
+    setSelectedDate(date);
+  };
+
+  return (
+    <div>
+      <h2>Select a Date:</h2>
+      <DatePicker selected={selectedDate} onChange={handleDateChange} />
+    </div>
+  );
+}
+
+export default Calendar;
