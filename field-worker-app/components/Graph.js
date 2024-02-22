63f8f0b8ea85411f6979727956133d7e1085e3cc
Farman Ahmed <farmanahmed888@gmail.com>
2024-02-22 13:22:31 +0530

    Update Graph.js

--- field-worker-app/components/Graph.js
+++ field-worker-app/components/Graph.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { View, StyleSheet } from "react-native";
-import { LineChart, Grid } from "react-native-svg-charts";
+// import { LineChart, Grid } from "react-native-svg-charts";
 
 const Graph = () => {
   const data = [50, 10, 40, 95, -4, -24, 85, 91, 35, 53, -53, 24, 50, -20, -80];
