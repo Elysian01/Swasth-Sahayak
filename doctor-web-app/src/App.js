5dc27b1824d114359d52fe5be7eb7528e6879964
Farman Ahmed <farmanahmed888@gmail.com>
2024-04-04 01:39:14 +0530

    Revert "protected route implemented"
    This reverts commit 44bb3b4274c8a645d5aca094d7ea6f8fbceae54e.

--- doctor-web-app/src/App.js
+++ doctor-web-app/src/App.js
@@ -17,103 +17,62 @@ import PatientDashboard from "./pages/PatientDashboard";
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Calendar from "./components/misc/Calendar";
-import ProtectedRoute from "./utils/ProtectedRoute";
 
 function App() {
-  const serverURL = "http://localhost:9191";
-  return (
-    <div className="App">
-      <Router>
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/forgot-password" element={<ForgotPassword />} />
-          <Route path="/reset-password" element={<ResetPassword />} />
-          <Route
-            path="search-patient"
-            element={
-              <ProtectedRoute>
-                <SearchPatient />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="search-records"
-            element={
-              <ProtectedRoute>
-                <SearchRecords />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="doctor-chat"
-            element={
-              <ProtectedRoute>
-                <DoctorChat />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="profile"
-            element={
-              <ProtectedRoute>
-                <Profile />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="diagnose-report"
-            element={
-              <ProtectedRoute>
-                <DiagnoseReport />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="diagnose-request"
-            element={
-              <ProtectedRoute>
-                <DiagnoseRequest />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="view-artifacts"
-            element={
-              <ProtectedRoute>
-                <Artifacts />
-              </ProtectedRoute>
-            }
-          ></Route>
-          <Route
-            path="/appointments"
-            element={
-              <ProtectedRoute>
-                <Appointments />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/doctor-dashboard"
-            element={
-              <ProtectedRoute>
-                <DoctorDashboard />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/patient-dashboard"
-            element={
-              <ProtectedRoute>
-                <PatientDashboard />
-              </ProtectedRoute>
-            }
-          />
-          <Route path="*" element={<PageNotFound />} />
-        </Routes>
-      </Router>
-    </div>
-  );
+	const serverURL = "http://localhost:9191";
+	return (
+		<div className="App">
+			<Router>
+				<Routes>
+					<Route path="/" element={<DoctorDashboard />} />
+					<Route path="/login" element={<Login />} />
+					<Route
+						path="/forgot-password"
+						element={<ForgotPassword />}
+					/>
+					<Route
+						path="/reset-password"
+						element={<ResetPassword />}
+					/>
+					<Route
+						path="search-patient"
+						element={<SearchPatient />}
+					/>
+					<Route
+						path="search-records"
+						element={<SearchRecords />}
+					/>
+					<Route path="doctor-chat" element={<DoctorChat />} />
+					<Route path="profile" element={<Profile />} />
+					<Route
+						path="diagnose-report"
+						element={<DiagnoseReport />}
+					/>
+					<Route
+						path="diagnose-request"
+						element={<DiagnoseRequest />}
+					/>
+					<Route
+						path="view-artifacts"
+						element={<Artifacts />}
+					></Route>
+					<Route
+						path="/appointments"
+						element={<Appointments />}
+					/>
+					<Route
+						path="/doctor-dashboard"
+						element={<DoctorDashboard />}
+					/>
+					<Route
+						path="/patient-dashboard"
+						element={<PatientDashboard />}
+					/>
+					<Route path="*" element={<PageNotFound />} />
+				</Routes>
+			</Router>
+		</div>
+	);
 }
 
 export default App;
