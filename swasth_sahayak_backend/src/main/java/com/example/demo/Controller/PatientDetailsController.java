31e05b80dd684768c64266190ecb11a3ab3b8811
Abhishek Gupta <abhig0209@gmail.com>
2024-05-03 20:43:46 +0530

    unnecessary files deleted

--- swasth_sahayak_backend/src/main/java/com/example/demo/Controller/PatientDetailsController.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.Controller;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-
-@RequestMapping("/patientDetails")
-public class PatientDetailsController {
-}
